d480a455d01d824fbd3f1a64a1a843dd5126301d
Filippo Valsorda <filippo@golang.org>
2026-06-15 15:03:30 +0200

    cmd/link: include build info in wasm binaries
    Fixes #73741
    
    Change-Id: I318defc5496f55dc6d22ca2e710a35b36a6a6964

--- src/cmd/link/internal/wasm/asm.go
+++ src/cmd/link/internal/wasm/asm.go
@@ -129,6 +129,7 @@ func asmb(ctxt *ld.Link, ldr *loader.Loader) {
 		ldr.SymSect(ldr.Lookup("runtime.itablink", 0)),
 		ldr.SymSect(ldr.Lookup("runtime.firstmoduledata", 0)),
 		ldr.SymSect(ldr.Lookup("runtime.pclntab", 0)),
+		ldr.SymSect(ldr.Lookup("go:buildinfo", 0)),
 		ldr.SymSect(ldr.Lookup("runtime.noptrdata", 0)),
 		ldr.SymSect(ldr.Lookup("runtime.data", 0)),
 	}
